76a9a0b81f2db697de9f31f00874a838ae414c10
lion <rhkwkdkrl@gmail.com>
2024-01-01 21:49:35 +0900

    주석 추가

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,18 +1,19 @@
 import { createClient } from "@sanity/client";
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false, // set to `false` to bypass the edge cache
-  apiVersion: '2023-10-25', // use current date (YYYY-MM-DD) to target the latest API version
-  token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
-})
+  apiVersion: "2023-10-25", // use current date (YYYY-MM-DD) to target the latest API version
+  token: process.env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
+});
 
-
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source).width(800).url();
 }
+
+// sanity.ts 만드는 과정 강의 12.20 : 12:48초부터 나옴
